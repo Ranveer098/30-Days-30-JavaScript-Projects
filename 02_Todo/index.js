81d768d656933030f9116c688a537c63381b3b28
Ranveer098 <95100082+Ranveer098@users.noreply.github.com>
2024-02-13 15:30:13 +0530

    init

--- 02_Todo/index.js
+++ 02_Todo/index.js
@@ -1,1 +1,12 @@
-alert("hey")
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li)
+  }
+}
